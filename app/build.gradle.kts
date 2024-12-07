801ef74d2ae4770b0436815a5c3870b928f6d198
shiven <shiven.career@proton.me>
2024-12-07 21:19:27 +0530

    Chore: fixed targetSDK API to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     defaultConfig {
         applicationId = "dcrustm.ecell.mobile"
         minSdk = 29
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
