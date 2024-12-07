637a2afb815af29c63337a88a388e54f41046983
shiven <shiven.career@proton.me>
2024-12-07 21:05:30 +0530

    Fixed: base structure

--- app/src/main/java/dcrustm/ecell/mobile/MainActivity.kt
+++ app/src/main/java/dcrustm/ecell/mobile/MainActivity.kt
@@ -4,16 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import dcrustm.ecell.mobile.ui.theme.ECellDCRUSTMTheme
+import dcrustm.ecell.mobile.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ECellDCRUSTMTheme {
-
-            }
-        }
+            AppTheme {  }
     }
 }
