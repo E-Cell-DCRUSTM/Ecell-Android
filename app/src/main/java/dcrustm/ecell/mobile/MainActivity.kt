a584acfc4377efd51a95ff6637f37bf7dc012ece
shiven <shiven.career@proton.me>
2024-12-07 20:44:29 +0530

    added initial files
    Setup an Android 12 API Level 31 files for the android app

---
+++ app/src/main/java/dcrustm/ecell/mobile/MainActivity.kt
@@ -0,0 +1,19 @@
+package dcrustm.ecell.mobile
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import dcrustm.ecell.mobile.ui.theme.ECellDCRUSTMTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ECellDCRUSTMTheme {
+
+            }
+        }
+    }
+}
